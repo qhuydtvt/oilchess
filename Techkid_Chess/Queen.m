9e398a919ced851229f98e5be219dbf479acb77f
qhuydtvt <qhuydtvt@gmail.com>
2016-06-05 18:59:13 +0700

    Check move for Queen

--- Techkid_Chess/Queen.m
+++ Techkid_Chess/Queen.m
@@ -10,6 +10,16 @@
 
 @implementation Queen
 
+- (BOOL) checkMoveWithRow:(int)nextRow Column:(int)nextColumn; {
+    if(![super checkMoveWithRow:nextRow Column:nextColumn]) return NO;
+    if(self.row == nextRow || self.column == nextColumn) {
+        return [self checkMoveInStraightLine:nextRow :nextColumn];
+    } else {
+        return [self checkMoveInDiagonalLine:nextRow :nextColumn];
+    }
+    
+}
+
 - (BOOL) checkMoveWithDeltaX:(int)deltaX deltaY:(int)deltaY; {
     return (deltaX == 0 || deltaY == 0 || (deltaX == deltaY));
 }
